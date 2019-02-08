c9736a0a7f36769732dd6e771d3c2bc5e43b708b
Miłosz Kaczmarek <miloszkaczmarek@hotmail.com>
2019-02-08 14:28:22 +0100

    Added axios and API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,11 +5,17 @@
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <div v-for="event in events">
+        <h5>{{event.name}}</h5>
+        <a :href="event.url" target="_blank">KUP BILET</a>
+      </div>
     </p>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
 
 export default {
   name: 'HelloWorld',
@@ -18,11 +24,18 @@ export default {
   },
   data: function(){
     return{
-      info: []
+      events: []
     }
   },
   methods: {
     getUsers: function() {
+      var vm = this;
+      axios
+        .get("http://app.ticketmaster.com/discovery/v2/events.json?apikey=\tv9Gbr2U1JUFwfeFQBEHmkEAtRZ8PkvxE&city=\"warsaw\"")
+        .then(function(response) {
+          vm.events = response.data._embedded.events
+          console.log(response.data._embedded.events);
+        });
     }
   },
   mounted(){
